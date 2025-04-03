5422ec8f9abf76390bc2acb31addac8c689ea436
Quentin Petton <quentin.petton@hotmail.fr>
2025-04-03 11:27:08 +0200

    feat: add heroComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
-import { Routes } from '@angular/router';
+import type { Routes } from '@angular/router';
+import { HeroComponent } from './components/home/hero/hero.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HeroComponent,
+  },
+];
