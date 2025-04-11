7cbf109c5afd59d368eb6fe49896298b75569024
Quentin Petton <quentin.petton@hotmail.fr>
2025-04-11 11:28:52 +0200

    feat: inject dependancy categoryService

--- src/app/components/home/categories/categories.component.ts
+++ src/app/components/home/categories/categories.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CategoryService } from '../../../services/category.service';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-categories',
@@ -6,4 +8,7 @@ import { Component } from '@angular/core';
   template: ' <p>categories works!</p> ',
   styles: '',
 })
-export class CategoriesComponent {}
+export class CategoriesComponent {
+  private readonly categoryService = inject(CategoryService);
+  readonly cetegories = toSignal(this.categoryService.getAllCategories());
+}
--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -1,20 +1,16 @@
+import type { Category } from './../models/category';
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 
-import type { ProductItems } from '../models/product-items';
-
 @Injectable({
   providedIn: 'root',
 })
-export class ProductService {
+export class CategoryService {
   private readonly http = inject(HttpClient);
   private readonly apiUrl = environment.apiUrl;
 
-  getAllProducts() {
-    return this.http.get<ProductItems[]>(`${this.apiUrl}/products`);
-  }
-  getProductBySlug(slug: string) {
-    return this.http.get<ProductItems>(`${this.apiUrl}/products/${slug}`);
+  getAllCategories() {
+    return this.http.get<Category[]>(`${this.apiUrl}/categories`);
   }
 }
