a267d9f178923c2ce8e2be822ce7d470e2e0883a
Quentin Petton <quentin.petton@hotmail.fr>
2025-04-11 11:03:14 +0200

    feat: add categories component

---
+++ src/app/components/home/categories/categories.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-categories',
+  imports: [],
+  template: ' <p>categories works!</p> ',
+  styles: '',
+})
+export class CategoriesComponent {}
