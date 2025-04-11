2f0783f947ee348e5a1b07cec1658de5ed456738
Quentin Petton <quentin.petton@hotmail.fr>
2025-04-11 11:45:19 +0200

    feat: add directive for categories

--- src/app/components/home/categories/categories.component.ts
+++ src/app/components/home/categories/categories.component.ts
@@ -1,14 +1,19 @@
 import { Component, inject } from '@angular/core';
 import { CategoryService } from '../../../services/category.service';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { CategoryComponent } from './category/category.component';
 
 @Component({
   selector: 'app-categories',
-  imports: [],
-  template: ' <p>categories works!</p> ',
+  imports: [CategoryComponent],
+  template: `
+    @for (category of categories(); track $index) {
+      <app-category></app-category>
+    }
+  `,
   styles: '',
 })
 export class CategoriesComponent {
   private readonly categoryService = inject(CategoryService);
-  readonly cetegories = toSignal(this.categoryService.getAllCategories());
+  readonly categories = toSignal(this.categoryService.getAllCategories());
 }
