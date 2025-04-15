71f3afd334e9248a89c2bc89567b6a86df36788c
Quentin Petton <quentin.petton@hotmail.fr>
2025-04-15 10:59:59 +0200

    feat: add style menuBurger

--- src/app/components/shared/uis/menu/menu.component.ts
+++ src/app/components/shared/uis/menu/menu.component.ts
@@ -3,7 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-menu',
   imports: [],
-  template: ` <p>menu works!</p> `,
+  template: `
+    <nav
+      class="absolute flex justify-between px-8 w-full items-center py-4 border-b border-grey-light/30"
+    >
+      <img src="/assets/shared/tablet/icon-hamburger.svg" alt="burger menu icon" />
+      <img src="/assets/shared/desktop/logo.svg" alt="logo audiophile" />
+      <img src="/assets/shared/desktop/icon-cart.svg" alt="cart icon" />
+    </nav>
+  `,
   styles: '',
 })
 export class MenuComponent {}
