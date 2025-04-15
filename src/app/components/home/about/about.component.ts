2858e0093ff7c096b1ce117e18a11db042d7a3ac
Quentin Petton <quentin.petton@hotmail.fr>
2025-04-15 09:58:30 +0200

    feat: add gome/aboutComponent

---
+++ src/app/components/home/about/about.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { UpperCasePipe } from '@angular/common';
+
+@Component({
+  selector: 'app-about',
+  imports: [UpperCasePipe],
+  template: `
+    <section>
+      <img
+        src="/assets/shared/desktop/image-best-gear.jpg"
+        alt="man listening music with headphones"
+        class="rounded-lg mx-8"
+      />
+
+      <h4>
+        {{ 'Bringing you the' | uppercase }} <span>{{ 'best' | uppercase }}</span>
+        {{ 'audio gear' | uppercase }}
+      </h4>
+      <p>
+        Located at the heart of New York City, Audiophile is the premier store for high end
+        headphones, earphones, speakers, and audio accessories. We have a large showroom and luxury
+        demonstration rooms available for you to browse and experience a wide range of our products.
+        Stop by our store to meet some of the fantastic people who make Audiophile the best place to
+        buy your portable audio equipment.
+      </p>
+    </section>
+  `,
+  styles: [''],
+})
+export class AboutComponent {}
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { HeroComponent } from './hero/hero.component';
 import { CategoriesComponent } from './categories/categories.component';
 import { FeaturedProductsComponent } from './featured-products/featured-products.component';
+import { AboutComponent } from './about/about.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeroComponent, CategoriesComponent, FeaturedProductsComponent],
+  imports: [HeroComponent, CategoriesComponent, FeaturedProductsComponent, AboutComponent],
   template: `
     <app-hero></app-hero>
     <app-categories></app-categories>
     <app-featured-products> </app-featured-products>
+    <app-about></app-about>
   `,
   styles: '',
 })
