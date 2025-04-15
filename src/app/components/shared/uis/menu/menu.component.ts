0d73f11ce0939ac5eff58229a4b4c77c6d0ef749
Quentin Petton <quentin.petton@hotmail.fr>
2025-04-15 16:33:52 +0200

    feat style when burgerIsOpen

--- src/app/components/shared/uis/menu/menu.component.ts
+++ src/app/components/shared/uis/menu/menu.component.ts
@@ -12,11 +12,15 @@ import { NavbarComponent } from './navbar.component';
         (toggleBurgerMenu)="toggleBurgerMenu()"
       ></app-navbar>
     } @else {
-      <app-categories></app-categories>
-      <app-navbar
-        [burgerMenuIsOpen]="burgerMenuIsOpen()"
-        (toggleBurgerMenu)="toggleBurgerMenu()"
-      ></app-navbar>
+      <div class="fixed inset-0 z-10 bg-black/50"></div>
+      <div class="absolute top-0 left-0 w-full flex flex-col z-40">
+        <app-navbar
+          [burgerMenuIsOpen]="burgerMenuIsOpen()"
+          (toggleBurgerMenu)="toggleBurgerMenu()"
+          class="z-50 bg-black pb-14"
+        ></app-navbar>
+        <app-categories class="bg-white pb-6 rounded-b-lg"></app-categories>
+      </div>
     }
   `,
   styles: '',
