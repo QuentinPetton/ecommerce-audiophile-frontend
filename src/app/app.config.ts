de622c334c548dc0fa40546fdb1f22ef662ea7d7
Quentin Petton <quentin.petton@hotmail.fr>
2025-04-15 10:43:35 +0200

    feat: add mainLayoutComponent

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,21 @@
-import { type ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideZoneChangeDetection, type ApplicationConfig } from '@angular/core';
 import { provideRouter, type Routes } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
+import { MainLayoutComponent } from './components/shared/layout/main-layout/main-layout.component';
 
-export const routes: Routes = [{ path: '', component: HomeComponent }];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent,
+      },
+    ],
+  },
+];
 
 export const appConfig: ApplicationConfig = {
   providers: [
