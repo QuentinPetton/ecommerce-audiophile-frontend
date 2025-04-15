8e7e93fe9863b81cce462d808004f73a4d5f5d2f
Quentin Petton <quentin.petton@hotmail.fr>
2025-04-15 10:48:29 +0200

    feat: add ui/menuComponent

--- src/app/components/shared/layout/main-layout/main-layout.component.ts
+++ src/app/components/shared/layout/main-layout/main-layout.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from '../../uis/menu/menu.component';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [RouterOutlet],
-  template: ' <router-outlet></router-outlet>',
+  imports: [RouterOutlet, MenuComponent],
+  template: ` <app-menu></app-menu>
+    <router-outlet></router-outlet>`,
   styles: '',
 })
 export class MainLayoutComponent {}
