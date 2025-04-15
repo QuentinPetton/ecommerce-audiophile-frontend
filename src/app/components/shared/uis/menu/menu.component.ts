e0081a909b2d3e97aecc8467fcc7239eb823ff71
Quentin Petton <quentin.petton@hotmail.fr>
2025-04-15 11:44:56 +0200

    feat: add togglerBurgerMenu method

--- src/app/components/shared/uis/menu/menu.component.ts
+++ src/app/components/shared/uis/menu/menu.component.ts
@@ -1,17 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   imports: [],
   template: `
-    <nav
-      class="absolute flex justify-between px-8 w-full items-center py-4 border-b border-grey-light/30"
-    >
-      <img src="/assets/shared/tablet/icon-hamburger.svg" alt="burger menu icon" />
-      <img src="/assets/shared/desktop/logo.svg" alt="logo audiophile" />
-      <img src="/assets/shared/desktop/icon-cart.svg" alt="cart icon" />
-    </nav>
+    @if (!burgerMenuIsOpen()) {
+      <nav
+        class="absolute flex justify-between px-8 w-full items-center py-4 border-b border-grey-light/30"
+      >
+        <button (click)="toggleBurgerMenu()" class="cursor-pointer">
+          <img src="/assets/shared/tablet/icon-hamburger.svg" alt="burger menu icon" />
+        </button>
+        <img src="/assets/shared/desktop/logo.svg" alt="logo audiophile" />
+        <img src="/assets/shared/desktop/icon-cart.svg" alt="cart icon" />
+      </nav>
+    } @else {
+      <p>test</p>
+    }
   `,
   styles: '',
 })
-export class MenuComponent {}
+export class MenuComponent {
+  burgerMenuIsOpen = signal(false);
+
+  toggleBurgerMenu() {
+    this.burgerMenuIsOpen.update((value) => !value);
+  }
+}
