4673f37fe7cd6882579219799dc48c2db22ab658
Quentin Petton <quentin.petton@hotmail.fr>
2025-04-17 14:33:14 +0200

    feat: add pipe in productDescription

--- src/app/components/product-details/product-description/product-description.component.ts
+++ src/app/components/product-details/product-description/product-description.component.ts
@@ -1,18 +1,25 @@
 import { Component, computed, input } from '@angular/core';
 import type { ProductItems } from '../../../models/product-items';
+import { UpperCasePipe } from '@angular/common';
+import { SplitBeforeCategoryPipe } from '../../../pipes/split-before-category.pipe';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-product-description',
-  imports: [],
+  imports: [UpperCasePipe, SplitBeforeCategoryPipe, CurrencyPipe],
   template: `
     @if (product()) {
-      <p>{{ product().description }}</p>
-
       <picture>
         <source [attr.srcset]="productMainImage()?.desktop" media="(min-width: 1024px)" />
         <source [attr.srcset]="productMainImage()?.tablet" media="(min-width: 768px)" />
         <img [src]="productMainImage()?.mobile" alt="Image of {{ product().slug }}" />
       </picture>
+      @if (product().new) {
+        <span>{{ 'new product' | uppercase }}</span>
+      }
+      <h1 class="whitespace-pre-line">{{ product().name | splitBeforeCategory | uppercase }}</h1>
+      <p>{{ product().description }}</p>
+      <span>{{ product().price | currency: 'USD' : 'symbol' : '1.0-0' : 'en' }}</span>
     }
   `,
   styles: '',
