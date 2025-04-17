840004bc63269d3d06fa0cb3a35f808e8b42cb17
Quentin Petton <quentin.petton@hotmail.fr>
2025-04-17 21:05:10 +0200

    style: productFeatureComponent & media queries

--- src/app/components/product-details/product-features/product-features.component.ts
+++ src/app/components/product-details/product-features/product-features.component.ts
@@ -5,25 +5,35 @@ import type { ProductItems } from '../../../models/product-items';
   selector: 'app-product-features',
   imports: [],
   template: `
-    <section class="mx-8 mb-8">
-      <h2 class="text-xl font-bold mb-8">Features</h2>
-      <p class="opacity-50">{{ product().features }}</p>
+    @if (product()) {
+      <section class="mx-8 mb-8">
+        <h2 class="text-xl font-bold mb-8">Features</h2>
+        <p class="opacity-50">{{ product().features }}</p>
 
-      <h2 class="text-xl font-bold mt-16 mb-6">In the box</h2>
-      <ul>
-        @for (product of productIncludes(); track $index) {
-          <li class="my-2">
-            <span class="text-orange mr-4">{{ product.quantity }}x</span>
-            <span class="opacity-50">
-              {{ product.item }}
-            </span>
-          </li>
+        <h2 class="text-xl font-bold mt-16 mb-6">In the box</h2>
+        <ul>
+          @for (product of productIncludes(); track $index) {
+            <li class="my-2">
+              <span class="text-orange mr-4">{{ product.quantity }}x</span>
+              <span class="opacity-50">
+                {{ product.item }}
+              </span>
+            </li>
+          }
+        </ul>
+        @for (image of productImageGallery(); track $index) {
+          <picture>
+            <source [attr.srcset]="image.imageDesktop" media="(min-width: 1024px)" />
+            <source [attr.srcset]="image.imageTablet" media="(min-width: 768px)" />
+          </picture>
+          <img
+            class="rounded-lg mb-4"
+            [src]="image.imageMobile"
+            alt="image of {{ product().name }}"
+          />
         }
-      </ul>
-      @for (image of productImageGallery(); track $index) {
-        <img [src]="image.imageDesktop" alt="" />
-      }
-    </section>
+      </section>
+    }
   `,
   styles: '',
 })
