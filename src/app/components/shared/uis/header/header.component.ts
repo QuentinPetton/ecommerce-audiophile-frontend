aee31df32c7716d65133cca83a910be74e6935ce
Quentin Petton <quentin.petton@hotmail.fr>
2025-05-07 10:43:48 +0200

    feat: add navbar desktop for header layout

--- src/app/components/shared/uis/header/header.component.ts
+++ src/app/components/shared/uis/header/header.component.ts
@@ -1,28 +1,34 @@
 import { Component, signal } from '@angular/core';
 import { CategoriesComponent } from '../../../home/categories/categories.component';
 import { BurgerMenuComponent } from './burgerMenu.component';
+import { NavbarDesktopComponent } from '../navbar/navbar-desktop/navbar-desktop.component';
 
 @Component({
   selector: 'app-header',
-  imports: [CategoriesComponent, BurgerMenuComponent],
+  imports: [CategoriesComponent, BurgerMenuComponent, NavbarDesktopComponent],
   template: `
     <header>
-      @if (!burgerMenuIsOpen()) {
-        <app-burgerMenu
-          [burgerMenuIsOpen]="burgerMenuIsOpen()"
-          (toggleBurgerMenu)="toggleBurgerMenu()"
-        ></app-burgerMenu>
-      } @else {
-        <div (click)="toggleBurgerMenu()" class="fixed inset-0 z-20 bg-black/50"></div>
-        <div class="absolute top-0 left-0 w-full flex flex-col z-40">
+      <div class="block lg:hidden">
+        @if (!burgerMenuIsOpen()) {
           <app-burgerMenu
             [burgerMenuIsOpen]="burgerMenuIsOpen()"
             (toggleBurgerMenu)="toggleBurgerMenu()"
-            class="z-50 bg-black pb-14"
           ></app-burgerMenu>
-          <app-categories class="bg-white pb-6 rounded-b-lg"></app-categories>
-        </div>
-      }
+        } @else {
+          <div (click)="toggleBurgerMenu()" class="fixed inset-0 z-20 bg-black/50"></div>
+          <div class="absolute top-0 left-0 w-full flex flex-col z-40">
+            <app-burgerMenu
+              [burgerMenuIsOpen]="burgerMenuIsOpen()"
+              (toggleBurgerMenu)="toggleBurgerMenu()"
+              class="z-50 bg-black pb-14"
+            ></app-burgerMenu>
+            <app-categories class="bg-white pb-6 rounded-b-lg"></app-categories>
+          </div>
+        }
+      </div>
+      <div class="hidden lg:block">
+        <app-navbar-desktop></app-navbar-desktop>
+      </div>
     </header>
   `,
   styles: '',
