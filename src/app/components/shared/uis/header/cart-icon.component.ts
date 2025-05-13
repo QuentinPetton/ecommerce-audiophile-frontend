372dabbece2eaa0fbdd751c048106846ad15a063
Quentin Petton <quentin.petton@hotmail.fr>
2025-05-13 22:30:30 +0200

    refacto: getCartItems() in carOpen component instead of cartIcon

--- src/app/components/shared/uis/header/cart-icon.component.ts
+++ src/app/components/shared/uis/header/cart-icon.component.ts
@@ -1,20 +1,15 @@
 import { Component, inject } from '@angular/core';
 import { CartService } from '../../../../services/cart.service';
-import { JsonPipe } from '@angular/common';
 import { CartOpenComponent } from './cart-open.component';
 
 @Component({
   selector: 'app-cart-icon',
-  imports: [JsonPipe, CartOpenComponent],
+  imports: [CartOpenComponent],
   template: `
     <img (click)="handleCartClick()" src="/assets/shared/desktop/icon-cart.svg" alt="cart icon" />
     @if (cartService.isCartOpen()) {
       <app-cart-open></app-cart-open>
     }
-    @if (cartItems().length === 0) {
-      <p>Votre panier est vide</p>
-    }
-    <pre>{{ cartItems() | json }}</pre>
   `,
   styles: '',
 })
--- src/app/components/shared/uis/header/cart-open.component.ts
+++ src/app/components/shared/uis/header/cart-open.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { CartService } from './../../../../services/cart.service';
+import { Component, inject } from '@angular/core';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-cart-open',
-  imports: [],
-  template: ` <p>cart-open works!</p> `,
+  imports: [JsonPipe],
+  template: ` <p>cart-open works!</p>
+    @if (cartItems().length === 0) {
+      <p>Votre panier est vide</p>
+    }
+    <pre>{{ cartItems() | json }}</pre>`,
   styles: '',
 })
-export class CartOpenComponent {}
+export class CartOpenComponent {
+  readonly CartService = inject(CartService);
+  readonly cartItems = this.CartService.getCartItems();
+}
