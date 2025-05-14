668e6a6b4111d5337045cc9b5fe4a1739bcf249a
Quentin Petton <quentin.petton@hotmail.fr>
2025-05-14 09:56:05 +0200

    feat(cart): add cart display and cartItems list

--- src/app/components/shared/uis/header/cart-open.component.ts
+++ src/app/components/shared/uis/header/cart-open.component.ts
@@ -1,15 +1,33 @@
 import { CartService } from './../../../../services/cart.service';
 import { Component, inject } from '@angular/core';
-import { JsonPipe } from '@angular/common';
+import { UpperCasePipe } from '@angular/common';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-cart-open',
-  imports: [JsonPipe],
-  template: ` <p>cart-open works!</p>
+  imports: [UpperCasePipe, CurrencyPipe],
+  template: `
     @if (cartItems().length === 0) {
       <p>Votre panier est vide</p>
     }
-    <pre>{{ cartItems() | json }}</pre>`,
+    <div>
+      <h2>{{ 'Cart' | uppercase }} ({{ cartItems().length }})</h2>
+      <button class="cursor-pointer">Remove all</button>
+      @for (cartItems of cartItems(); track $index) {
+        <div>
+          <h3>{{ cartItems.slug | uppercase }}</h3>
+          <span>{{ cartItems.price | currency: 'USD' : 'symbol' : '1.0-0' : 'en' }}</span>
+          <div>
+            <button class="cursor-pointer">-</button>
+            <span>{{ cartItems.quantity }}</span>
+            <button class="cursor-pointer">+</button>
+          </div>
+        </div>
+        <span>{{ 'Total' | uppercase }}</span>
+        <button class="cursor-pointer">{{ 'Checkout' | uppercase }}</button>
+      }
+    </div>
+  `,
   styles: '',
 })
 export class CartOpenComponent {
