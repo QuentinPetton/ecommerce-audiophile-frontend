51c974ea7dcfb2dc09d8581f88f47cad8c5c8af4
Quentin Petton <quentin.petton@hotmail.fr>
2025-05-19 15:30:19 +0200

    style(cart): responsive layout

--- src/app/components/shared/uis/header/cart-open.component.ts
+++ src/app/components/shared/uis/header/cart-open.component.ts
@@ -11,27 +11,31 @@ import { CurrencyPipe } from '@angular/common';
       <p>Votre panier est vide</p>
     }
     <div
-      class="fixed right-30 top-20 max-h-[80vh] w-full max-w-[377px] bg-white text-black p-6 shadow-2xl rounded-lg z-50 overflow-y-auto"
+      class="fixed inset-0 z-50 flex justify-center items-start mt-20 px-4 md:justify-end lg:mr-10"
     >
-      <h2>{{ 'Cart' | uppercase }} ({{ cartItems().length }})</h2>
-      <button (click)="removeAllItemsFromCart()" class="cursor-pointer">Remove all</button>
-      <button (click)="toggleCart()" class=" text-red-700  px-6">x</button>
-      @for (cartItems of cartItems(); track $index) {
-        <div>
-          <img [src]="cartItems.image" alt="Image of {{ cartItems.slug }}" />
-          <h3>{{ cartItems.slug | uppercase }}</h3>
-          <span>{{ cartItems.price | currency: 'USD' : 'symbol' : '1.0-0' : 'en' }}</span>
+      <div
+        class=" top-20 max-h-[80vh] w-full max-w-[377px] bg-white text-black p-6 shadow-2xl rounded-lg z-50 overflow-y-auto"
+      >
+        <h2>{{ 'Cart' | uppercase }} ({{ cartItems().length }})</h2>
+        <button (click)="removeAllItemsFromCart()" class="cursor-pointer">Remove all</button>
+        <button (click)="toggleCart()" class=" text-red-700  px-6">x</button>
+        @for (cartItems of cartItems(); track $index) {
           <div>
-            <button (click)="removeOneQuantityFromCart(cartItems.id)" class="cursor-pointer">
-              -
-            </button>
-            <span>{{ cartItems.quantity }}</span>
-            <button (click)="addOneQuantityToCart(cartItems.id)" class="cursor-pointer">+</button>
+            <img [src]="cartItems.image" alt="Image of {{ cartItems.slug }}" />
+            <h3>{{ cartItems.slug | uppercase }}</h3>
+            <span>{{ cartItems.price | currency: 'USD' : 'symbol' : '1.0-0' : 'en' }}</span>
+            <div>
+              <button (click)="removeOneQuantityFromCart(cartItems.id)" class="cursor-pointer">
+                -
+              </button>
+              <span>{{ cartItems.quantity }}</span>
+              <button (click)="addOneQuantityToCart(cartItems.id)" class="cursor-pointer">+</button>
+            </div>
           </div>
-        </div>
-        <span>{{ 'Total' | uppercase }}</span>
-        <button class="cursor-pointer">{{ 'Checkout' | uppercase }}</button>
-      }
+          <span>{{ 'Total' | uppercase }}</span>
+          <button class="cursor-pointer">{{ 'Checkout' | uppercase }}</button>
+        }
+      </div>
     </div>
   `,
   styles: '',
