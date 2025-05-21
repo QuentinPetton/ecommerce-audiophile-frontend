c2dc2b6461d8c8f11ec7ba03a482910341495152
Quentin Petton <quentin.petton@hotmail.fr>
2025-05-21 22:16:53 +0200

    feat(category-products): add component

---
+++ src/app/components/home/categories/category-products/category-products.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-category-products',
+  imports: [],
+  template: `
+    <p>
+      category-products works!
+    </p>
+  `,
+  styles: ``
+})
+export class CategoryProductsComponent {
+
+}
