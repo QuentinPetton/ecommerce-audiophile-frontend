035f7809232087acc6a3fd03d6be85406b9bd3e7
Quentin Petton <quentin.petton@hotmail.fr>
2025-05-22 11:15:27 +0200

    feat(category-products): display products by category slug

--- src/app/components/home/categories/category-products/category-products.component.ts
+++ src/app/components/home/categories/category-products/category-products.component.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ProductService } from '../../../../services/product.service';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { map, switchMap } from 'rxjs';
+import { ProductDescriptionComponent } from '../../../product-details/product-description/product-description.component';
 
 @Component({
   selector: 'app-category-products',
-  imports: [],
-  template: ` <p>category-products works!</p> `,
+  imports: [ProductDescriptionComponent],
+  template: `
+    @for (product of products(); track $index) {
+      <app-product-description [productSignal]="product"></app-product-description>
+    }
+  `,
   styles: '',
 })
-export class CategoryProductsComponent {}
+export class CategoryProductsComponent {
+  private readonly route = inject(ActivatedRoute);
+  private readonly productService = inject(ProductService);
+  readonly products = toSignal(
+    this.route.paramMap.pipe(
+      map((params) => params.get('slug') as string),
+      switchMap((slug) => this.productService.getProductsByCategorySlug(slug)),
+    ),
+  );
+  //TODO: isoler productDescription button pour avoir un see more depuis catégories
+}
