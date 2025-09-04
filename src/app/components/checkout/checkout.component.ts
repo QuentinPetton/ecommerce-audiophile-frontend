46de84cb3e3409d708f90834b304cfc37b609158
Quentin Petton <quentin.petton@hotmail.fr>
2025-09-04 11:34:42 +0200

    feat: add billin detals form

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,15 +1,30 @@
+import { UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
-  imports: [],
+  imports: [ReactiveFormsModule, UpperCasePipe],
   template: `
-    <p>
-      checkout works!
-    </p>
+    <main>
+      <h1>{{ 'Checkout' | uppercase }}</h1>
+      <form [formGroup]="checkoutForm">
+        <section>
+          <h2>{{ 'Billing details' | uppercase }}</h2>
+          <div>
+            <label for="name">Name</label>
+            <input type="text" formControlName="name" placeholder="Name" />
+            <label for="email">Email</label>
+            <input type="email" formControlName="email" placeholder="Email" />
+            <label for="phone">Phone Number</label>
+            <input type="tel" formControlName="phone" placeholder="+1(202) 555-0136" />
+          </div>
+        </section>
+      </form>
+    </main>
   `,
-  styles: ``
+  styles: '',
 })
 export class CheckoutComponent {
-
+  readonly checkoutForm = new FormGroup({});
 }
