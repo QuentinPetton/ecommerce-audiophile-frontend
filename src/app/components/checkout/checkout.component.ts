26877e0df34a3624eaa0652e5764c891a3a56e7f
Quentin Petton <quentin.petton@hotmail.fr>
2025-09-04 11:56:06 +0200

    feat: add shipping info & validator for checkoutForm

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -20,6 +20,34 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
             <input type="tel" formControlName="phone" placeholder="+1(202) 555-0136" />
           </div>
         </section>
+        <section>
+          <h2>{{ 'Shipping Info' | uppercase }}</h2>
+          <div>
+            <label for="address">Address</label>
+            <input type="text" id="address" placeholder="1137 Williams Avenue" />
+            <label for="zip">ZIP Code</label>
+            <input type="text" id="zip" placeholder="10001" />
+            <label for="city">City</label>
+            <input type="text" id="city" placeholder="New York" />
+            <label for="country">Country</label>
+            <input type="text" id="country" placeholder="United States" />
+          </div>
+        </section>
+        <section>
+          <h2>{{ 'Shipping Info' | uppercase }}</h2>
+          <div>
+            <label for="payement_method">Payement Method</label>
+            <input type="radio" id="e-money" name="payement_method" checked />
+            <label for="e-money">e-Money</label>
+            <input type="radio" id="cash" name="payement_method" />
+            <label for="cash">Cash on Delivery</label>
+            <p>
+              The 'Cash on Delivery' option enables you to pay in cash when our delivery courier
+              arrives at your residence. Just make sure your address is correct so that your order
+              will not be cancelled.
+            </p>
+          </div>
+        </section>
       </form>
     </main>
   `,
@@ -34,5 +62,10 @@ export class CheckoutComponent {
     ]),
     email: new FormControl('', [Validators.required, Validators.email]),
     phone: new FormControl('', [Validators.required, Validators.minLength(10)]),
+    adress: new FormControl('', [Validators.required]),
+    zip: new FormControl('', [Validators.required]),
+    city: new FormControl('', [Validators.required]),
+    country: new FormControl('', [Validators.required]),
+    payement_method: new FormControl('e-money', [Validators.required]),
   });
 }
